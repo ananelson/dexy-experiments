18d9142db0d93e870e0010d2dbaffe68603ea590
Ana Nelson <ana@ananelson.com>
2014-09-30 15:45:24 -0700

    Remove me-specific -v setting.

--- plant-uml/run-docker.sh
+++ plant-uml/run-docker.sh
@@ -2,5 +2,4 @@ set -e
 docker build -t ananelson/dexy-plantuml .
 docker run -t -i \
     -v `pwd`:/home/repro/work \
-    -v ~/dev/dexy-two:/home/repro/dexy \
     ananelson/dexy-plantuml /bin/bash
